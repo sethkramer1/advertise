0004291a572d08104ecc1017fb54f2dafd7f4f64
Seth Kramer <sethaepiuva@gmail.com>
2018-10-13 12:35:30 -0400

    assets enabled in production

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,13 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
+# Enable the asset pipeline
+config.assets.enabled = true
+
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
